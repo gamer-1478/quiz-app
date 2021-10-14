d9818096ebf609a89521466230a21e96d5421c4e
Aayush Garg <74775129+gamer-1478@users.noreply.github.com>
2021-10-14 16:09:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Quiz from './quiz/QuizLoad'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div style={{ minHeight: '78.1vh' }}>
+          <Switch>
+            <Route path={process.env.PUBLIC_URL + '/'} exact component={Quiz} />
+          </Switch>
+        </div>
+      </Router>
     </div>
+
   );
 }
 
